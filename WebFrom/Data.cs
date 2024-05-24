66aebd989b8b02b34add2308d19da1f1cda0cf61
Anna2985 <hshiting58@gamil.com>
2024-05-24 16:18:33 +0800

    加入專案檔案。

---
+++ WebFrom/Data.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFrom
+{
+    public class Data
+    {
+       public paging paging { get; set; }
+       public Entry[] entries { get; set; }
+
+    }
+    public class paging
+    {
+        public int limit { get; set; }
+        public int offset { get; set; }
+        public int total { get; set; }
+    }
+    public class Entry
+    {
+        public string title { get; set; }
+        public string category { get; set; }
+        public int top { get; set; }
+        public string publication_date { get; set; }
+        public string takendown_date { get; set; }
+        public string app_publication_data { get; set; }
+        public string app_takendown_data { get; set; }
+        public string constants { get; set; }
+        public string remark { get; set; }
+        public Link[] links { get; set; }
+        public File[] files { get; set; }
+
+    }
+    public class Link
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+    }
+    public class File
+    {
+        public string filename { get; set; }
+        public string url { get; set; }
+
+    }
+}
